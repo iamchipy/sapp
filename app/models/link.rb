696575510c56cb496c1287451c845e49497101ac
John-David Basson <hire@johndavidbasson.com>
2025-05-15 16:43:32 -0500

    Link Model validation

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,16 @@
 class Link < ApplicationRecord
+  validates :original_url, presence: true, url: true # You'll need a 'url' validation gem later, or simple check
+  validates :short_code, presence: true, uniqueness: true
+
+  before_validation :generate_short_code, on: :create
+
+  private
+
+  def generate_short_code
+    # Basic code generation (can be made more robust)
+    self.short_code = loop do
+      code = SecureRandom.alphanumeric(6) # Needs 'securerandom' gem
+      break code unless Link.exists?(short_code: code)
+    end
+  end
 end
