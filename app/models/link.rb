14894a3de7b841f362cacdb9e50e28b0d829b5ae
John-David Basson <hire@johndavidbasson.com>
2025-05-16 14:50:35 -0500

    Found the key issue with Validation gaps

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,7 +2,7 @@ class Link < ApplicationRecord
   validates :original_url, presence: true, url: true
   validates :short_code, presence: true, uniqueness: true
 
-  before_validation :generate_short_code, on: :create
+  before_create :generate_short_code
   before_save :set_short_url
 
   def full_short_url
@@ -16,10 +16,6 @@ class Link < ApplicationRecord
   end
 
   def generate_short_code
-    # Basic code generation (can be made more robust)
-    self.short_code = loop do
-      code = SecureRandom.alphanumeric(6) # Needs 'securerandom' gem
-      break code unless Link.exists?(short_code: code)
-    end
+    self.short_code ||= SecureRandom.alphanumeric(6).upcase
   end
 end
