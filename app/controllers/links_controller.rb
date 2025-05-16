48f7f2ef073e0b880e690c95a21b8616cee61e77
John-David Basson <hire@johndavidbasson.com>
2025-05-16 13:08:22 -0500

    changed how #show picks filter

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -10,12 +10,12 @@ class LinksController < ApplicationController
   def create
     @link = Link.new(link_params) # Use strong parameters
 
-    # if @link.save
-    #   redirect_to @link, notice: "Link was successfully shortened." # Redirect on success
-    #   THIS WAS FAILING because default for @link without more is ->> /links/:id
-    #   :id was not being supported below
     if @link.save
-      redirect_to short_link_path(@link.short_code), notice: 'Link was successfully shortened.'
+      redirect_to @link, notice: "Link was successfully shortened." # Redirect on success
+    #   THIS WAS FAILING because default for @link without more is ->> /links/:id
+    #   :id was not being supported below in the SHOW method
+    # if @link.save
+    #   redirect_to short_link_path(@link.short_code), notice: 'Link was successfully shortened.'
     else
       render :new # Render the new template on failure (shows errors)
     end
@@ -25,13 +25,14 @@ class LinksController < ApplicationController
     @link = if params[:short_code]
       Link.find_by!(short_code: params[:short_code])
     else
-      Link.find(params[:id])
+      Link.find_by!(id: params[:id])
     end
   end
 
 
   private # Use private for helper methods not exposed as actions
 
+  # Validation method to make sure we are given a URL in the input
   def link_params
     params.require(:link).permit(:original_url) # Strong parameters
     # TODO investigate further
