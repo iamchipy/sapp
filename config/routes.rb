0c74f369288d76bf2ae21f030f6f3bdd7f3e1fad
John-David Basson <hire@johndavidbasson.com>
2025-05-17 14:46:06 -0500

    more production checks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => "/api-docs"
-  mount Rswag::Api::Engine => "/api-docs"
+  # Mount Rswag UI only in development and test environments
+  if Rails.env.development? || Rails.env.test?  
+    mount Rswag::Ui::Engine => "/api-docs"
+    mount Rswag::Api::Engine => "/api-docs"
+  end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # WEB interfaces
